a234c612cf5230f2fc6319c28d50ac763df309a2
Akın Elmas <36002989+akin-elmas@users.noreply.github.com>
2021-06-23 20:17:00 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,11 @@ function HomePage() {
         <div className="space-y-4">
           <h1 className="text-2xl font-bold">Selamlar, ben Akın.</h1>
 
-          <p>Front-End teknolojileri ile ilgileniyorum. Fmv Işık Üniversitesi Görsel İletişim Tasarımı mezunuyum. </p>
+          <p>Front-End teknolojileri ile ilgileniyorum.<br> Fmv Işık Üniversitesi Görsel İletişim Tasarımı mezunuyum. </p>
 
           <p>Uzun zamandır soyut ve somut tasarım çıktılarının olduğu işler yapmaktayım. Serigrafi, Litografi gibi baskı teknikleri ile boş zamanlarımda hobi amaçlı, ihtiyacım olduğu zamanlarda ticari olarak ürünler ortaya çıkartıyorum. Geleneksel tasarımın dışında web, web tipografisi ve kullanıcı arayüzü tasarımı alanlarıyla ilgileniyorum.</p>
-          <a href="https://docs.google.com/document/d/1bu3Edfo3PFMoyoxqpBcEFlKnUpsrsd2BectDYir3vgM/edit">CV</a>
+          <br>br>
+          <a href="https://docs.google.com/document/d/1bu3Edfo3PFMoyoxqpBcEFlKnUpsrsd2BectDYir3vgM/edit">CV Linki</a>
         </div>
       </div>
       <div className="site-4xl-container mt-20">
